a36857cc7bfa47fddf48bf44bc61ce1cc677b244
Matthew Denner <matt.denner@gmail.com>
2013-06-21 18:36:19 +0100

    Updated leiningen dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lobos "1.0.0-beta1" :exclusions [org.clojure/java.jdbc]]
                  [korma               "0.3.0-RC5"]
-                 [com.h2database/h2   "1.3.170"]
+                 [com.h2database/h2   "1.3.172"]
                  ]
-  :profiles  {:dev  {:dependencies  [[midje "1.5.0"]]}})
+  :profiles  {:dev  {:dependencies  [[midje "1.5.1"]]}})
